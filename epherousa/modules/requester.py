bb9b5727a3d69564b41f8055bc7ded8a3c2062ca
Sinderella <rawsec@gmail.com>
2017-01-04 04:29:55 +0000

    Remove warnings when SSL certs are ignored

--- epherousa/modules/requester.py
+++ epherousa/modules/requester.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from requests import Session
 from requests.adapters import HTTPAdapter
 
+from requests.packages.urllib3 import disable_warnings
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
 
 class Requester(Session):
     _USER_AGENT = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:45.0) Gecko/20100101 Firefox/45.0'}
@@ -27,6 +30,8 @@ class Requester(Session):
 
         self.mount('http://', HTTPAdapter(max_retries=Requester._MAX_RETRIES))
         self.mount('https://', HTTPAdapter(max_retries=Requester._MAX_RETRIES))
+
+        disable_warnings(InsecureRequestWarning)
 
     def get(self, url, **kwargs):
         return super(Requester, self).get(url, proxies=self.proxies, verify=self.verify_ssl, **kwargs)
