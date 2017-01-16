62e412990cbf23c6ce5dbefcdd213557f6959809
Sinderella <rawsec@gmail.com>
2017-01-16 07:39:28 +0000

    Fix Python 2 screaming about encoding at tests

--- epherousa/test/base_test.py
+++ epherousa/test/base_test.py
@@ -3,9 +3,17 @@
 
 from __future__ import absolute_import
 
+import sys
 import unittest
 
 
 class BaseTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(BaseTest, self).__init__(*args, **kwargs)
+
+        # Fix encoding problem in python 2.7
+        try:
+            reload(sys)
+            sys.setdefaultencoding('utf-8')
+        except NameError as e:
+            pass
